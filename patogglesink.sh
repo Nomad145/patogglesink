1773c227802936b2cfcf6b7ab5ffb23c9fe61adb
Michael Phillips <michaeljoelphillips@gmail.com>
2014-09-21 19:44:39 -0500

    Added options to include script in BAR configuration.

--- patogglesink.sh
+++ patogglesink.sh
@@ -8,10 +8,33 @@ SINKS=$(pacmd list-sinks | grep index | awk -F":" '{print $2}')
 ACTIVE=$(pacmd list-sinks | grep "* index" | awk '{print $3}')
 INACTIVE=$(pacmd list-sinks | grep "^ \{4,6\}index" | awk '{print $2}')
 
-pacmd set-default-sink $INACTIVE > /dev/null
+query() {
+	if [ $ACTIVE -eq "0" ]; then
+		echo "Headphones"
+	elif [ $ACTIVE -eq "1" ]; then
+		echo "Speakers"
+	fi
+}
 
-if [ -n $INPUTS ]; then
+toggle() {
+	pacmd set-default-sink $INACTIVE > /dev/null
+
 	for i in $INPUTS; do
 		pacmd move-sink-input $i $INACTIVE > /dev/null
 	done
-fi
+}
+
+case "$1" in
+	query)
+		query
+		;;
+	-q)
+		query
+		;;
+	toggle)
+		toggle
+		;;
+	-t)
+		toggle
+		;;
+esac
